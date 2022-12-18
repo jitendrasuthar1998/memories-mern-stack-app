4786639e405afbcc2f04e482dd5848296630fcd4
Jitendra Suthar <jitendra.suthar@growcify.com>
2022-12-19 00:40:32 +0530

    created individual Post component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { Post } from './Post/Post'
 import useStyles from './postsStyle';
-
+import { CircularProgress, Grid } from '@material-ui/core';
 export const Posts = () => {
     const classes = useStyles();
 
     const posts = useSelector((state) => state.posts)
     console.log("posts at Posts is == ", posts)
     return (
-        <div>
-            <Post />
-            <Post />
-            <Post />
-        </div>
+        !posts.length ? <CircularProgress /> : (
+            <Grid container className={classes.mainContainer} alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid item xs={12} sm={6} key={post._id}>
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
+
+
     )
 }
